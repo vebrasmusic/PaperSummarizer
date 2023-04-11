e0b097713bb562ef84330836958203b1bc4cc7d0
vebrasmusic <128623334+vebrasmusic@users.noreply.github.com>
2023-04-11 13:39:34 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+const pdfInput = document.getElementById("pdfInput");
+const htmlOutput = document.getElementById("htmlOutput");
+
+pdfInput.addEventListener("change", async (event) => {
+  const file = event.target.files[0];
+  if (!file) return;
+
+  const formData = new FormData();
+  formData.append("pdf", file);
+
+  try {
+    // Replace "YOUR_BACKEND_API_URL" with the actual URL of your backend API on the Vultr server.
+    const response = await fetch("YOUR_BACKEND_API_URL", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok) throw new Error("Failed to convert PDF to HTML.");
+
+    const htmlContent = await response.text();
+    htmlOutput.innerHTML = htmlContent;
+  } catch (error) {
+    console.error(error);
+    htmlOutput.innerHTML = "<p>Error: Failed to convert PDF to HTML.</p>";
+  }
+});
